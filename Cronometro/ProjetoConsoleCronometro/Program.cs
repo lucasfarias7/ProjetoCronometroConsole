1b587250dc67095d4042d008ef628e4a8760b469
Lucas Farias <lucasfarias14@outlook.com>
2022-09-11 19:14:02 -0300

    alterações no program.cs

--- Cronometro/ProjetoConsoleCronometro/Program.cs
+++ Cronometro/ProjetoConsoleCronometro/Program.cs
@@ -2,14 +2,61 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ProjetoConsoleCronometro
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            
+            Console.WriteLine(Menu());
+            Console.WriteLine("Quanto tempo desejar contar? ");
+
+            string dado = Console.ReadLine().ToLower();
+            char type = char.Parse(dado.Substring(dado.Length - 1, 1));
+            int time = int.Parse(dado.Substring(0, dado.Length - 1));
+            int multiplicador = 1;
+
+            if (type == 'm')
+                multiplicador = 60;
+
+            if(time == 0)
+                System.Environment.Exit(0);
+
+
+            StartCronometro(time * multiplicador);
+            
+            Console.ReadLine();
+        }
+
+        static string Menu()
+        {
+            Console.Clear();
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Menu");
+            sb.AppendLine("S = segundos => 10s = 10 segundos");
+            sb.AppendLine("M = Minutos => 10m = 10 minutos");
+            sb.AppendLine("0 = Sair");
+
+            return sb.ToString();
+        }
+
+        public static void StartCronometro(int temporizador)
+        {
+            int TempoAtual = 0;
+
+            while(TempoAtual != temporizador)
+            {
+                TempoAtual++;
+                Console.Clear();
+                Console.WriteLine(TempoAtual);
+                Thread.Sleep(1000);
+            }
+
+            Console.WriteLine("Cronometro finalizado!");
         }
     }
 }
